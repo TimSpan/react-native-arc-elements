42ff1acd3f99f1aa3782f7a1eb163f3ce5667750
TimSpan <cat20201314@163.com>
2026-05-18 17:30:15 +0800

    。。

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  extends: ['expo'],
+  rules: {
+    'object-curly-spacing': ['error', 'never'],
+    'curly-spacing': ['error', {arraysInObjects: false, objectsInObjects: false}],
+  },
+};
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,26 +1,11 @@
 // .prettierrc.js
 module.exports = {
-  // 箭头函数只有一个参数时省略括号 (x => x)
   arrowParens: 'avoid',
-
-  // 多行 JSX 的 > 换到下一行
   bracketSameLine: false,
-
-  // 对象字面量 {foo: bar} 是否加空格
-  bracketSpacing: true,
-
-  // 使用单引号
+  bracketSpacing: false,
   singleQuote: true,
-
-  // 多行时尽量在末尾加逗号
   trailingComma: 'all',
-
-  // 一行最大长度调大，避免 import / if / 函数调用换行
   printWidth: 200,
-
-  // Markdown 文本不自动换行
   proseWrap: 'never',
-
-  // JSX 内部使用单引号
   jsxSingleQuote: true,
 };
